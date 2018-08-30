9085dd92e2d0076194a944a1486dad5e0a63a2aa
chihiro <chanchi0707@gmail.com>
2018-08-30 14:50:13 +0900

    Initial Commit

---
+++ chanchihiro-acceptance/chanchihiro_acceptanceView.h
@@ -0,0 +1,13 @@
+//
+//  chanchihiro_acceptanceView.h
+//  chanchihiro-acceptance
+//
+//  Created by noguchi-chihiro on 2018/08/30.
+//  Copyright © 2018年 noguchi-chihiro. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface chanchihiro_acceptanceView : ScreenSaverView
+
+@end
